cb5dfcb24a74444d9084f9aa8626195b43fcf341
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-09 14:55:28 +0100

    add exercise comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,3 +1,31 @@
+//  2-Objektorientierung/9-Record
+
+//  Programmierung: Java Record Zoo Projekt
+//  Erstelle ein neues Projekt in IntelliJ und lege den Grundstein für das Java Record Zoo Projekt.
+//  Erstelle einen Java Record “Animal”, der Eigenschaften für ID, Name, Spezies und Alter haben soll. Wähle passende Feldnamen.
+//
+//  Programmierung: Record Methoden aufrufen
+//  Lass uns nun die automatisch generierten Methoden testen.
+//  Erstelle in einer Main-Methode mehrere Tiere.
+//  Rufe die automatisch generierten Methoden im Record auf und prüfe, ob sie das tun, was du erwartest (equals, toString).
+
+//  Programmierung: Verschachtelung
+//  Jetzt werden wir einen Record als Attribut eines anderen Records verwenden.
+//  Erstelle einen Owner Record, der einige Eigenschaften eines Haustierbesitzers speichert (Name, Alter, Adresse).
+//  Lass jedes Animal einen Owner haben.
+
+//  Programmierung: Verschachtelung + Wiederverwendung
+//  Verwenden eines Records in mehreren anderen Records.
+//  Erstelle einen Species Record, der die Eigenschaften einer Tierart speichert (Name, Futterbedarf in Gramm pro Tag).
+//  Lass jedes Animal eine Species haben.
+//  Lass in deiner Main-Methode mehrere Animal-Objekte die gleiche Species haben.
+
+//  Wenn du diese Aufgabe bereits früher abgeschlossen hast, kannst du die Bonusaufgabe auf der nächsten Seite bearbeiten.
+//  Bonus: Zoo
+//  Wenn du die Hauptaufgabe bereits abgeschlossen hast, kannst du diese Bonusaufgabe versuchen.
+//  Implementiere einen “Zoo” Record, der eine Liste aller Tiere hat und den gesamten Futterbedarf aller Tiere im Zoo berechnen kann.
+
+
 package org.example;
 
 
