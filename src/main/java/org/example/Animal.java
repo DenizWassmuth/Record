e21f8fa3b5ab4a6b1c7e62108603f8026e328631
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-11 20:12:02 +0100

    general cleanup

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,4 +2,7 @@ package org.example;
 
 public record Animal(int id, Species species, String name, int age, Owner owner) {
 
+    public Animal withName(String name){
+        return new Animal(id, species, name, age, owner);
+    }
 }
