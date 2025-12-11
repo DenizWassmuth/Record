371bda61e2dd90df37e6419abc7e6cecca22e1cf
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-11 16:12:45 +0100

    general cleanup

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,29 +2,4 @@ package org.example;
 
 public record Animal(int id, Species species, String name, int age, Owner owner) {
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Animal animal = (Animal) o;
-//        return id == animal.id && age == animal.age
-//                && Objects.equals(name, animal.name)
-//                && Objects.equals(species, animal.species)
-//                && Objects.equals(owner, animal.owner);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id, species, name, age, owner);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Animal{" +
-//                "id=" + id +
-//                ", species='" + species + '\'' +
-//                ", name='" + name + '\'' +
-//                ", age=" + age +
-//                ", owner=" + owner +
-//                '}';
-//    }
 }
