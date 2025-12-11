3c5893343a3dc95265c925c95c166fbef658774e
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-11 10:40:25 +0100

    change list to map and fix logic

--- src/main/java/org/example/Zoo.java
+++ src/main/java/org/example/Zoo.java
@@ -1,25 +1,38 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public record Zoo() {
 
-    static List<Animal> animals = new ArrayList<>();
+    static Map<Integer, Animal> animals = new HashMap<>();
 
     void addAnimal(Animal animal)
     {
-        animals.add(animal);
+        if (animal == null)
+        {
+            return;
+        }
+
+        if (animals.containsKey(animal.age()))
+        {
+            return;
+        }
+
+        animals.put(animal.id(), animal);
     }
 
    void calculateFoodNecessary() {
+
         if (animals.isEmpty()) {
             return;
         }
 
         float totalFood = 0;
 
-        for (Animal animal : animals) {
+        for (Animal animal : animals.values()) {
 
             System.out.println(animal.name() + ", species " +  animal.species().nameOfSpecies() + " needs " + animal.species().foodInGrams() + " food per day.");
 
